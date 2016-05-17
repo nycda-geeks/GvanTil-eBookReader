b6a85302ce7ddbceb7a1015ca981e8a5ef543d2e
Gijs van Til <gijsvantil@gmail.com>
2016-05-17 21:23:03 +0200

    created the get request on / with readfile

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require ('fs')
-
 var app = express()
 
-app.use(express.static('Public'));
+app.use(express.static('./resources'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,8 +13,16 @@ app.set ('view engine', 'pug');
 
 // GET REQUEST
 
-app.get('/', function(request, response){
-	response.render ('index');
+app.get('/', (request, response)=>{
+	fs.readFile ('./resources/books.json', function(error, data){
+		if (error){
+			throw error
+		} else {
+			var parsedBooks = JSON.parse (data)]
+			console.log (parsedBooks)
+			res.render ('index', { books:parsedBooks })
+		}
+	})
 });
 
 // SERVER SET UP
