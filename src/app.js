2682b7e7a3c8903a0a843f582370f8c17801aa08
Gijs van Til <gijsvantil@gmail.com>
2016-05-17 21:15:46 +0200

    added pictures to public and made a pug.file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,24 @@ var bodyParser = require('body-parser');
 var fs = require ('fs')
 
 var app = express()
+
+app.use(express.static('Public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set ('views', './src/views');
-app.set ('view engine', 'jade');
+app.set ('view engine', 'pug');
+
+
+// GET REQUEST
+
+app.get('/', function(request, response){
+	response.render ('index');
+});
+
+// SERVER SET UP
+
+app.listen(3000, function () {
+  console.log('eBookReader listening on port 3000');
+});
 
